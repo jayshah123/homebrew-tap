a8f696e002f1ece8fe48b50f22447235c6a6c291
jay shah <jayshah67@gmail.com>
2026-06-02 10:04:46 +0530

    mitmproxy-controller 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mitmproxy-controller.rb
+++ Formula/mitmproxy-controller.rb
@@ -1,13 +1,13 @@
 class MitmproxyController < Formula
   desc "System tray controller for mitmproxy"
   homepage "https://github.com/jayshah123/mitmproxy-controller"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jayshah123/mitmproxy-controller/releases/download/v0.2.0/mitmproxy-controller_darwin_arm64.tar.gz"
-      sha256 "74c750b2107fc2a7f96cce0ab5f160aef2e583d7b781cc700889aa106a2684a5"
+      url "https://github.com/jayshah123/mitmproxy-controller/releases/download/v0.3.0/mitmproxy-controller_darwin_arm64.tar.gz"
+      sha256 "41bd5e1a5a50ed0419ce4c535c8f70ad40617b15fd28961b393be76426942d27"
     end
   end
 
